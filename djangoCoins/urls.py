1637ebfd72d0ad3433c255765cf88f6a0280f8cc
Rinat Khantimirov <khantimirov.rinat@gmail.com>
2021-03-16 20:20:57 +0300

    Удалил админку за ненадобностью

--- djangoCoins/urls.py
+++ djangoCoins/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('coins.urls')),
 ]
 
